ee65d47a7aff037917fce3429ca1198c63bed0f6
pigpgw <ceh20002@naver.com>
2024-03-16 22:53:04 +0900

    ✅ 추가: 헤더에 로고 이미지와 Kodchasan 폰트 추가

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import '../fonts/font.css';
+import appLogo from '../assets/images/appLogo.png';
 
-function Header(){
-    return <>헤더입니다.</>
+const HeaderContainer = styled.div`
+  display: flex;
+  margin-top: 10px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+`;
+
+const LogoImage = styled.img`
+  width: 40px;
+  height: 40px;
+  margin: 15px 0 0px 12px;
+`;
+
+const LogoTitle = styled.p`
+  font-weight: 400;
+  width: 91px;
+  height: 26px;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  color: black;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <LogoContainer>
+        <LogoImage src={appLogo} alt="" />
+        <LogoTitle>OHNEUL</LogoTitle>
+      </LogoContainer>
+    </HeaderContainer>
+  );
 }
 
-export default Header
+export default Header;
