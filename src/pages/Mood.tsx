903667f84d77e4edd7f770444c5e02a22fa1c244
pigpgw <ceh20002@naver.com>
2024-03-17 02:03:15 +0900

    🚀 삭제: 불필요한 코드 삭제함

--- src/pages/Mood.tsx
+++ src/pages/Mood.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function Mood() {
-  const navigate = useNavigate();
-
-  const navigateMood = () => {
-      navigate("/mood")
-  }
   return (
     <>
       <p>Mood page입니다.</p>
-      <button onClick={navigateMood}></button>
     </>
   );
 }
