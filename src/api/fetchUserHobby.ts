c8a62cf59b7a65aecac79de9bb9fce50494183b9
pigpgw <ceh20002@naver.com>
2024-03-20 19:56:59 +0900

    ✅ 추가 : fetchGetHobby 함수에 타입추가

--- src/api/fetchUserHobby.ts
+++ src/api/fetchUserHobby.ts
@@ -1,9 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-export const fetchGetHobbys = async () => {
+interface Hobby {
+    hobby_id: number;
+    hobby: string;
+  }
+
+export const fetchGetHobbys = async ():Promise<Hobby[]> => {
   try {
-    const response = await axios.get('http://localhost:3000/hobby');
+    const response = await axios.get<Hobby[]>('http://localhost:3000/hobby');
     return response.data;
   } catch (e) {
     throw new Error('전체 취미 목록 불러오기 실패');
