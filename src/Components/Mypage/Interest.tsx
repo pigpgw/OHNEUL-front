fb853ab8a63a6b0701b6a4fe3a9abb0a8d1b77c9
hanur92 <rkdgksdjfwkd92@gmail.com>
2024-04-09 01:36:35 +0900

    ✨ 마이페이지 axios
    기본구조

--- src/Components/Mypage/Interest.tsx
+++ src/Components/Mypage/Interest.tsx
@@ -1,6 +1,21 @@
+import axios from 'axios';
 import React from 'react';
+import meltedCookie from 'utils/meltedCookie';
 
 const Interest = () => {
+  const [userId, token, flatform] = meltedCookie();
+  const fetchPayments = async () => {
+    axios
+      .get(`http://localhost:4000/payments/${userId}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return <div>Interest</div>;
 };
 
--- src/Components/Mypage/StarRating.tsx
+++ src/Components/Mypage/StarRating.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import meltedCookie from 'utils/meltedCookie';
+
 const StarRating = (name: any) => {
-  // axios.get(() => {});
+  const [userId, token, flatform] = meltedCookie();
+  const fetchPayments = async () => {
+    axios
+      .get(`http://localhost:4000/payments/${userId}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return <div>StarRating</div>;
 };
 
