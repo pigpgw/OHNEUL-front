ce98533b04c11f9641a39b49b1de447465e1cbce
hanur92 <rkdgksdjfwkd92@gmail.com>
2024-04-11 18:53:14 +0900

    ✨ StarRating
    데이터 전송만 받음녀 끝

--- src/Components/Mypage/StarRating.tsx
+++ src/Components/Mypage/StarRating.tsx
@@ -1,22 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import meltedCookie from 'utils/meltedCookie';
+import plusHeart from 'assets/images/plusHeart.png';
+import minusHeart from 'assets/images/minusHeart.png';
+import styled from 'styled-components';
 
-const StarRating = (name: any) => {
+const Heart = styled.img`
+  width: 20px;
+`;
+const Desc = styled.div`
+  font-size: 13px;
+  color: #dbdbdb;
+`;
+
+interface Item {
+  id: number;
+  score: number;
+}
+
+const StarRating = () => {
   const [userId, token, flatform] = meltedCookie();
-  const fetchPayments = async () => {
-    axios
-      .get(`http://localhost:4000/payments/${userId}`, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const [data, setData] = useState<Item[] | number>(0);
+  useEffect(() => {
+    const fetchPayments = async () => {
+      axios
+        .get(
+          `http://localhost:
+        4000/users/${userId}`,
+          {},
+        )
+        .then((res) => {
+          console.log(res);
+          setData(res.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    fetchPayments();
+  }, []);
+
+  const renderHerat = (score: number) => {
+    const plusCount = score;
+    const minusCount = 5 - score;
+    const count = [];
+    const desc = [];
+    for (let i = 0; i < plusCount; i += 1) {
+      count.push(<Heart src={plusHeart} alt="ph"></Heart>);
+    }
+    for (let j = 0; j < minusCount; j += 1) {
+      count.push(<Heart src={minusHeart} alt=""></Heart>);
+    }
+    switch (score) {
+      case 1:
+        desc.push(<Desc>별이한개!!!!!!</Desc>);
+        break;
+      case 2:
+        desc.push(<Desc>별이두개!!!!!!!</Desc>);
+        break;
+      case 3:
+        desc.push(<Desc>별이세개!!!!!!!!</Desc>);
+        break;
+      case 4:
+        desc.push(<Desc>별이두개!!!!!!!!</Desc>);
+        break;
+      case 5:
+        desc.push(<Desc>별이다섯개!!!!!!!!!</Desc>);
+        break;
+      default:
+        break;
+    }
+    return [desc, count];
   };
-  return <div>StarRating</div>;
+
+  return <div>{renderHerat(5)}</div>;
 };
 
 export default StarRating;
