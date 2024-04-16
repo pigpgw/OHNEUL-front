dee115fe5af71e1d1ddeaaa852f230c44a012361
pigpgw <ceh20002@naver.com>
2024-04-16 12:44:42 +0900

    ✅ 토스페이먼츠 결제 위젯 연결

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -5,16 +5,29 @@ import styled from 'styled-components';
 type PaymentWidget = {
   renderPaymentMethods: (selector: string, options: any, config: any) => any;
   renderAgreement: (selector: string, options: any) => any;
+  requestPayment: (paymentInfo: PaymentInfo) => void;
 };
 
+type PaymentInfo = {
+  orderId: string;
+  orderName: string;
+  amount: number;
+  successUrl: string;
+  failUrl: string;
+};
+
+
 function Checkout() {
-  const cliuent = process.env.REACT_APP_TOSS_PAYMENTS_CLIENT_KEY;
+  const clientKey = process.env.REACT_APP_TOSS_PAYMENTS_CLIENT_KEY as string;
   const paymentWidgetRef = useRef<PaymentWidget | null>(null);
   const paymentMethodsWidgetRef = useRef<any>(null);
   const agreementWidgetRef = useRef<any>(null);
-  const [price, setPrice] = useState<number>(1000);
-  const clientKey = 'test_ck_vZnjEJeQVxK4P0RDgevd3PmOoBN0';
+  const [price, setPrice] = useState<number>(500);
+  //   const clientKey = 'test_ck_vZnjEJeQVxK4P0RDgevd3PmOoBN0';
   const customerKey = '4a394e47-64a1-44de-91c2-6bcd30d72c57'; // 내 상점에서 고객을 구분하기 위해 발급한 고객의 고유 ID
+  const generateRandomString = (): string => {
+    return Math.random().toString(36).substr(2, 10);
+  };
 
   useEffect(() => {
     (async () => {
@@ -42,19 +55,50 @@ function Checkout() {
   }, []);
 
   return (
-    <PaymentsWrapper className="payment-method">
-      <PaymentsWrapper className="agreement"></PaymentsWrapper>
-    </PaymentsWrapper>
+    <>
+      <PaymentsWrapper className="payment-method">
+        <PaymentsWrapper className="agreement"></PaymentsWrapper>
+      </PaymentsWrapper>
+      <PaymentBtn
+        onClick={async () => {
+          const paymentWidget = paymentWidgetRef.current;
+
+          try {
+            await paymentWidget?.requestPayment({
+              orderId: generateRandomString(),
+              orderName: '토스 티셔츠 외 2건',
+              amount: price,
+              successUrl: `${window.location.origin}/success`,
+              failUrl: `${window.location.origin}/fail`,
+            });
+          } catch (error) {
+            // TODO: 에러 처리
+          }
+        }}
+      >
+        결제하기
+      </PaymentBtn>
+    </>
   );
 }
 
 export default Checkout;
 
 const PaymentsWrapper = styled.div`
-    &.payment-method {
-        width: 100%;
-    }
-    &.agreement {
-        width: 100%;
-    }
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PaymentBtn = styled.button`
+  width: 70%;
+  height: 40px;
+  color: white;
+  margin: 10px;
+  background: #0075ff;
+  border-radius: 5px;
+  border: 0;
+  max-width: 500px;
+  margin-top: 100px;
 `;
