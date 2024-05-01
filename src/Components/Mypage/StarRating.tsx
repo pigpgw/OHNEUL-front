3bc610f931a89876fb69f547b306da138fa62b98
pigpgw <ceh20002@naver.com>
2024-05-02 06:28:37 +0900

    💄 마이페이지 . 별수정

--- src/Components/Mypage/StarRating.tsx
+++ src/Components/Mypage/StarRating.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaStar } from '@react-icons/all-files/fa/FaStar';
+import { TiStarFullOutline } from '@react-icons/all-files/ti/TiStarFullOutline';
+import { TiStarHalfOutline } from '@react-icons/all-files/ti/TiStarHalfOutline';
+import { TiStarOutline } from '@react-icons/all-files/ti/TiStarOutline';
+
 import meltedCookie from 'utils/meltedCookie';
 import plusHeart from 'assets/images/plusHeart.png';
 import minusHeart from 'assets/images/minusHeart.png';
@@ -16,7 +19,7 @@ const Heart = styled.img`
 const Desc = styled.div`
   font-size: 2vh;
   font-weight: 600;
-  color: #000000;
+  color: #ffe598;
   margin: 10px;
   @media (max-width: 768px) {
     font-size: 2vh;
@@ -26,13 +29,7 @@ const Desc = styled.div`
 const StarRating = () => {
   const [flatform, token, rewardCoin, userId] = meltedCookie();
   const [data, setData] = useState<number>(0);
-  const [star, setStar] = useState<boolean[]>([
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
+  const [star, setStar] = useState<number[]>([0, 0, 0, 0, 0]); // 2가 풀 1 반
   const li: { [key: number]: string } = {
     5: '멋쟁이 잰틀맨',
     4: '신사',
@@ -41,51 +38,57 @@ const StarRating = () => {
     1: 'ㅠㅠ',
   };
 
-  const fetchScore: any = async () => {
-    const response = await axios.get(
-      `${process.env.REACT_APP_BASE_URL}/users/${userId}`,
-    );
-    const { score } = response.data;
-    console.log('ㄴ책ㄷ',score)
-    const myScore = Number(score)
-    if (myScore >= 4.5) {
-      setData(5);
-    } else if (myScore >= 3.5) {
-      setData(4);
-      console.log('점수')
-    } else if (myScore >= 2.5) {
-      setData(3);
-      console.log('점수')
-    } else if (myScore >= 1.5) {
-      setData(2);
-    } else if (myScore >= 0.5) {
-      setData(1);
-    }
+  const fetchScore = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/users/${userId}`,
+      );
+      const { score } = response.data;
+      console.log('Score:', score);
+      const myScore = Number(score);
+      let roundedScore = Math.round(myScore * 2) / 2; 
+      if (roundedScore > 5) {
+        roundedScore = 5;
+      } else if (roundedScore < 0) {
+        roundedScore = 0;
+      }
 
-    const clickStates = [...star];
-    for (let i = 0; i < data; i += 1) {
-      clickStates[i] = true;
+      setData(roundedScore);
+
+      const starStates: number[] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 5; i++) {
+        if (roundedScore >= i + 1) starStates.push(2);
+        else if (roundedScore === i + 0.5) starStates.push(1);
+        else starStates.push(0);
+      }
+      setStar(starStates);
+    } catch (error) {
+      console.log('유저 점수 가져오기 실패:', error);
     }
-    setStar(clickStates);
   };
+
   useEffect(() => {
     fetchScore();
-    console.log(data)
-  }, [data]);
+  }, []);
 
   return (
     <>
       <P>당신의 매너점수</P>
       <Wrap>
         <Stars>
-          {[0, 1, 2, 3, 4].map((el, idx) => {
-            return (
-              <FaStar
-                key={idx}
-                size="40"
-                className={star[el] ? 'yellowStar' : ''}
-              />
-            );
+          {star.map((starState, idx) => {
+            if (starState === 2) {
+              return <TiStarFullOutline key={idx} size="50" />;
+            }
+            if (starState === 1) {
+              return (
+                <div key={idx}>
+                  <TiStarHalfOutline key={idx} size="50" />
+                </div>
+              );
+            }
+            return <TiStarOutline key={idx} size="50" />;
           })}
         </Stars>
       </Wrap>
@@ -105,14 +108,7 @@ const Stars = styled.div`
   justify-content: space-between;
   width: 100%;
   padding: 10px 10px 2vh 0;
-
-  & svg {
-    color: gray;
-  }
-
-  .yellowStar {
-    color: #ffe598;
-  }
+  color: #ffe598;
 `;
 
 const P = styled.p`
