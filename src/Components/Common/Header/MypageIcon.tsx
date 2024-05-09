65128d265b98529cfb6bf2a78b68408010ff63f6
pigpgw <ceh20002@naver.com>
2024-05-09 16:30:44 +0900

    💄 Mypage icon vh -> px 로 수정

--- src/Components/Common/Header/MypageIcon.tsx
+++ src/Components/Common/Header/MypageIcon.tsx
@@ -14,7 +14,7 @@ function MypageIcon() {
 export default React.memo(MypageIcon);
 
 const IconPerson = styled(IoPersonOutline)`
-  font-size: 28px;
-  margin: 1.5vh 10px 5px 10px;
+  font-size: 27px;
+  margin: 15px 10px 5px 10px;
   color: gray;
 `;
